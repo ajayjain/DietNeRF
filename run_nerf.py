Fix gradient checkpointing bug

run_nerf.py
@@ -208,8 +208,7 @@ def create_nerf(args):
     skips = [4]
     model = NeRF(D=args.netdepth, W=args.netwidth,
                  input_ch=input_ch, output_ch=output_ch, skips=skips,
-                 input_ch_views=input_ch_views, use_viewdirs=args.use_viewdirs,
-                 checkpoint=args.checkpoint_rendering)
+                 input_ch_views=input_ch_views, use_viewdirs=args.use_viewdirs)
     model = nn.DataParallel(model).to(device)
     wandb.watch(model)
     grad_vars = list(model.parameters())
@@ -218,8 +217,7 @@ def create_nerf(args):
     if args.N_importance > 0:
         model_fine = NeRF(D=args.netdepth_fine, W=args.netwidth_fine,
                           input_ch=input_ch, output_ch=output_ch, skips=skips,
-                          input_ch_views=input_ch_views, use_viewdirs=args.use_viewdirs,
-                          checkpoint=args.checkpoint_rendering)
+                          input_ch_views=input_ch_views, use_viewdirs=args.use_viewdirs)
         model_fine = nn.DataParallel(model_fine).to(device)
         grad_vars += list(model_fine.parameters())
 
@@ -727,8 +725,13 @@ def train():
     network_fn = render_kwargs_train['network_fn']
     network_fine = render_kwargs_train['network_fine']
     if args.checkpoint_rendering:
-        render_kwargs_train['network_fn'] = lambda x: torch.utils.checkpoint.checkpoint(network_fn, x)
-        render_kwargs_train['network_fine'] = lambda x: torch.utils.checkpoint.checkpoint(network_fine, x)
+        # Pass a dummy input tensor that requires grad so checkpointing does something
+        # https://discuss.pytorch.org/t/checkpoint-with-no-grad-requiring-inputs-problem/19117/10
+        dummy = torch.ones(1, dtype=torch.float32, requires_grad=True, device=device)
+        network_fn_wrapper = lambda x, y: network_fn(x)
+        network_fine_wrapper = lambda x, y: network_fine(x)
+        render_kwargs_train['network_fn'] = lambda x: torch.utils.checkpoint.checkpoint(network_fn_wrapper, x, dummy)
+        render_kwargs_train['network_fine'] = lambda x: torch.utils.checkpoint.checkpoint(network_fine_wrapper, x, dummy)
 
 
     bds_dict = {

run_nerf_helpers.py
@@ -74,7 +74,7 @@ def get_embedder(multires, i=0):
 
 # Model
 class NeRF(nn.Module):
-    def __init__(self, D=8, W=256, input_ch=3, input_ch_views=3, output_ch=4, skips=[4], use_viewdirs=False, checkpoint=False):
+    def __init__(self, D=8, W=256, input_ch=3, input_ch_views=3, output_ch=4, skips=[4], use_viewdirs=False):
         """ 
         """
         super(NeRF, self).__init__()
@@ -84,8 +84,6 @@ class NeRF(nn.Module):
         self.input_ch_views = input_ch_views
         self.skips = skips
         self.use_viewdirs = use_viewdirs
-        # self.checkpoint = checkpoint
-        self.checkpoint = False  # debug: trying a coarser strategy
 
         self.pts_linears = nn.ModuleList(
             [DenseLayer(input_ch, W, activation="relu")] + [DenseLayer(W, W, activation="relu") if i not in self.skips else DenseLayer(W + input_ch, W, activation="relu") for i in range(D-1)])
@@ -108,32 +106,19 @@ class NeRF(nn.Module):
         input_pts, input_views = torch.split(x, [self.input_ch, self.input_ch_views], dim=-1)
         h = input_pts
         for i, l in enumerate(self.pts_linears):
-            if self.checkpoint:
-                # TODO: use a smarter strategy than recomputing all linear layers
-                h = torch.utils.checkpoint.checkpoint(self.pts_linears[i], h)
-                h = torch.utils.checkpoint.checkpoint(F.relu, h)
-            else:
-                h = self.pts_linears[i](h)
-                h = F.relu(h)
+            h = self.pts_linears[i](h)
+            h = F.relu(h)
             if i in self.skips:
                 h = torch.cat([input_pts, h], -1)
 
         if self.use_viewdirs:
-            if self.checkpoint:
-                alpha = torch.utils.checkpoint.checkpoint(self.alpha_linear, h)
-                feature = torch.utils.checkpoint.checkpoint(self.feature_linear, h)
-            else:
-                alpha = self.alpha_linear(h)
-                feature = self.feature_linear(h)
+            alpha = self.alpha_linear(h)
+            feature = self.feature_linear(h)
             h = torch.cat([feature, input_views], -1)
         
             for i, l in enumerate(self.views_linears):
-                if self.checkpoint:
-                    h = torch.utils.checkpoint.checkpoint(self.views_linears[i], h)
-                    h = torch.utils.checkpoint.checkpoint(F.relu, h)
-                else:
-                    h = self.views_linears[i](h)
-                    h = F.relu(h)
+                h = self.views_linears[i](h)
+                h = F.relu(h)
 
             rgb = self.rgb_linear(h)
             outputs = torch.cat([rgb, alpha], -1)