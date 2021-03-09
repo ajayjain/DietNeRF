7829382932421e42f7aa03f506ecb0284c1fbe34
Ajay Jain <ajayj@berkeley.edu>
2021-03-09 10:51:08 -0800

    Add 4 view test script

--- scripts/run_synthetic_nerf_4views.sh
+++ scripts/run_synthetic_nerf_4views.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-PYTHONPATH=/data/ajay/clip/CLIP:$PYTHONPATH CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/220_blender_chair_4views.txt &
-PYTHONPATH=/data/ajay/clip/CLIP:$PYTHONPATH CUDA_VISIBLE_DEVICES=6 python run_nerf.py --config configs/221_blender_drums_4views.txt &
-PYTHONPATH=/data/ajay/clip/CLIP:$PYTHONPATH CUDA_VISIBLE_DEVICES=7 python run_nerf.py --config configs/222_blender_ficus_4views.txt &
+# PYTHONPATH=/data/ajay/clip/CLIP:$PYTHONPATH CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/220_blender_chair_4views.txt &
+# PYTHONPATH=/data/ajay/clip/CLIP:$PYTHONPATH CUDA_VISIBLE_DEVICES=6 python run_nerf.py --config configs/221_blender_drums_4views.txt &
+# PYTHONPATH=/data/ajay/clip/CLIP:$PYTHONPATH CUDA_VISIBLE_DEVICES=7 python run_nerf.py --config configs/222_blender_ficus_4views.txt &
 
 # PYTHONPATH=/data/ajay/clip/CLIP:$PYTHONPATH CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/223_blender_lego_4views.txt &
 # PYTHONPATH=/data/ajay/clip/CLIP:$PYTHONPATH CUDA_VISIBLE_DEVICES=4 python run_nerf.py --config configs/224_blender_mic_4views.txt &
 # PYTHONPATH=/data/ajay/clip/CLIP:$PYTHONPATH CUDA_VISIBLE_DEVICES=5 python run_nerf.py --config configs/225_blender_ship_4views.txt &
+
+
+## Test
+CUDA_VISIBLE_DEVICES=2 python run_nerf.py --render_only --render_test --config configs/223_blender_lego_4views.txt &
+CUDA_VISIBLE_DEVICES=3 python run_nerf.py --render_only --render_test --config configs/224_blender_mic_4views.txt &
+wait;
+CUDA_VISIBLE_DEVICES=2 python run_nerf.py --render_only --render_test --config configs/225_blender_ship_4views.txt &
+
+# CUDA_VISIBLE_DEVICES=3 --render_only --render_test python run_nerf.py --config configs/220_blender_chair_4views.txt &
+# CUDA_VISIBLE_DEVICES=6 --render_only --render_test python run_nerf.py --config configs/221_blender_drums_4views.txt &
+# CUDA_VISIBLE_DEVICES=7 --render_only --render_test python run_nerf.py --config configs/222_blender_ficus_4views.txt &
