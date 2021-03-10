49e9ff5615896bc6302d33d2db7a7982884fc423
Ajay Jain <ajayj@berkeley.edu>
2021-03-09 19:47:49 -0800

    Add blender materials and hotdog configs

--- scripts/run_synthetic_nerf_8views_ctr.sh
+++ scripts/run_synthetic_nerf_8views_ctr.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+## Train
+# Running on b7_1
 # CUDA_VISIBLE_DEVICES=0 python run_nerf.py --config configs/250_blender_chair_8views_ctr.txt &
 # CUDA_VISIBLE_DEVICES=1 python run_nerf.py --config configs/251_blender_drums_8views_ctr.txt &
+
+# Running on pabdgx-1
 # CUDA_VISIBLE_DEVICES=2 python run_nerf.py --config configs/252_blender_ficus_8views_ctr.txt &
-# CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/253_blender_lego_8views_ctr.txt &
 # CUDA_VISIBLE_DEVICES=4 python run_nerf.py --config configs/254_blender_mic_8views_ctr.txt &
+
+# Need to run
 # CUDA_VISIBLE_DEVICES=5 python run_nerf.py --config configs/255_blender_ship_8views_ctr.txt &
+
+# Skip (using earlier run 042 for lego)
+# CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/253_blender_lego_8views_ctr.txt &
 
 ## Test with 8 views
 CUDA_VISIBLE_DEVICES=0 python run_nerf.py --render_only --render_test --config configs/250_blender_chair_8views_ctr.txt &
