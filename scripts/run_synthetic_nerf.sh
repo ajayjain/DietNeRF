b5ac75f29f67422647b6f86e4cd0480d1b1348cf
Ajay Jain <ajayj@berkeley.edu>
2021-03-09 21:05:57 -0800

    Hotdog and materials baseline configs

--- scripts/run_synthetic_nerf.sh
+++ scripts/run_synthetic_nerf.sh
@@ -5,12 +5,14 @@
 # PYTHONPATH=/data/ajay/clip/CLIP:$PYTHONPATH CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/lego.txt &
 # PYTHONPATH=/data/ajay/clip/CLIP:$PYTHONPATH CUDA_VISIBLE_DEVICES=4 python run_nerf.py --config configs/mic.txt &
 # PYTHONPATH=/data/ajay/clip/CLIP:$PYTHONPATH CUDA_VISIBLE_DEVICES=5 python run_nerf.py --config configs/ship.txt &
+CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/hotdog.txt &
+CUDA_VISIBLE_DEVICES=4 python run_nerf.py --config configs/materials.txt &
 
 ## Test
-CUDA_VISIBLE_DEVICES=0 python run_nerf.py --render_only --render_test --config configs/lego.txt &
-CUDA_VISIBLE_DEVICES=1 python run_nerf.py --render_only --render_test --config configs/mic.txt &
-CUDA_VISIBLE_DEVICES=2 python run_nerf.py --render_only --render_test --config configs/ship.txt &
-wait;
-CUDA_VISIBLE_DEVICES=0 python run_nerf.py --render_only --render_test --config configs/chair.txt &
-CUDA_VISIBLE_DEVICES=1 python run_nerf.py --render_only --render_test --config configs/drums.txt &
-CUDA_VISIBLE_DEVICES=2 python run_nerf.py --render_only --render_test --config configs/ficus.txt &
+# CUDA_VISIBLE_DEVICES=0 python run_nerf.py --render_only --render_test --config configs/lego.txt &
+# CUDA_VISIBLE_DEVICES=1 python run_nerf.py --render_only --render_test --config configs/mic.txt &
+# CUDA_VISIBLE_DEVICES=2 python run_nerf.py --render_only --render_test --config configs/ship.txt &
+# wait;
+# CUDA_VISIBLE_DEVICES=0 python run_nerf.py --render_only --render_test --config configs/chair.txt &
+# CUDA_VISIBLE_DEVICES=1 python run_nerf.py --render_only --render_test --config configs/drums.txt &
+# CUDA_VISIBLE_DEVICES=2 python run_nerf.py --render_only --render_test --config configs/ficus.txt &
