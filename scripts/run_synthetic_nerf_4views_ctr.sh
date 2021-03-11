2c3ec4a6533fb276eb0ee3f19d79ef1dc590bf8e
Ajay Jain <ajayj@berkeley.edu>
2021-03-10 17:48:05 -0800

    Update testing commands

--- scripts/run_synthetic_nerf_4views_ctr.sh
+++ scripts/run_synthetic_nerf_4views_ctr.sh
@@ -9,16 +9,19 @@
 # CUDA_VISIBLE_DEVICES=5 python run_nerf.py --config configs/247_blender_materials_4views_ctr.txt &
 
 # With GAN
-CUDA_VISIBLE_DEVICES=0 python run_nerf.py --config configs/266_blender_hotdog_4views_ctr_gan.txt &
-CUDA_VISIBLE_DEVICES=1 python run_nerf.py --config configs/276_blender_hotdog_4views_ctr_gan1.txt &
-CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/267_blender_materials_4views_ctr_gan.txt &
-CUDA_VISIBLE_DEVICES=2 python run_nerf.py --config configs/277_blender_materials_4views_ctr_gan1.txt &
+# CUDA_VISIBLE_DEVICES=0 python run_nerf.py --config configs/266_blender_hotdog_4views_ctr_gan.txt &
+# CUDA_VISIBLE_DEVICES=1 python run_nerf.py --config configs/276_blender_hotdog_4views_ctr_gan1.txt &
+# CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/267_blender_materials_4views_ctr_gan.txt &
+# CUDA_VISIBLE_DEVICES=2 python run_nerf.py --config configs/277_blender_materials_4views_ctr_gan1.txt &
 
 ## Test with 4 views
-CUDA_VISIBLE_DEVICES=0 python run_nerf.py --render_only --render_test --config configs/243_blender_lego_4views_ctr.txt &
-CUDA_VISIBLE_DEVICES=1 python run_nerf.py --render_only --render_test --config configs/244_blender_mic_4views_ctr.txt &
-CUDA_VISIBLE_DEVICES=2 python run_nerf.py --render_only --render_test --config configs/245_blender_ship_4views_ctr.txt &
+CUDA_VISIBLE_DEVICES=3 python run_nerf.py --render_only --render_test --config configs/246_blender_hotdog_4views_ctr.txt &
+CUDA_VISIBLE_DEVICES=4 python run_nerf.py --render_only --render_test --config configs/247_blender_materials_4views_ctr.txt &
+CUDA_VISIBLE_DEVICES=5 python run_nerf.py --render_only --render_test --config configs/243_blender_lego_4views_ctr.txt &
 wait;
-CUDA_VISIBLE_DEVICES=0 python run_nerf.py --render_only --render_test --config configs/240_blender_chair_4views_ctr.txt &
-CUDA_VISIBLE_DEVICES=1 python run_nerf.py --render_only --render_test --config configs/241_blender_drums_4views_ctr.txt &
-CUDA_VISIBLE_DEVICES=2 python run_nerf.py --render_only --render_test --config configs/242_blender_ficus_4views_ctr.txt &
+CUDA_VISIBLE_DEVICES=3 python run_nerf.py --render_only --render_test --config configs/244_blender_mic_4views_ctr.txt &
+CUDA_VISIBLE_DEVICES=4 python run_nerf.py --render_only --render_test --config configs/245_blender_ship_4views_ctr.txt &
+CUDA_VISIBLE_DEVICES=5 python run_nerf.py --render_only --render_test --config configs/240_blender_chair_4views_ctr.txt &
+wait;
+CUDA_VISIBLE_DEVICES=3 python run_nerf.py --render_only --render_test --config configs/241_blender_drums_4views_ctr.txt &
+CUDA_VISIBLE_DEVICES=4 python run_nerf.py --render_only --render_test --config configs/242_blender_ficus_4views_ctr.txt &
--- scripts/run_synthetic_nerf_8views_ctr.sh
+++ scripts/run_synthetic_nerf_8views_ctr.sh
@@ -14,6 +14,14 @@
 # CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/253_blender_lego_8views_ctr.txt &
 
 ## Test with 8 views
-CUDA_VISIBLE_DEVICES=0 python run_nerf.py --render_only --render_test --config configs/250_blender_chair_8views_ctr.txt &
-CUDA_VISIBLE_DEVICES=1 python run_nerf.py --render_only --render_test --config configs/251_blender_drums_8views_ctr.txt &
-CUDA_VISIBLE_DEVICES=2 python run_nerf.py --render_only --render_test --config configs/042_blender_paper_lego.txt &
+CUDA_VISIBLE_DEVICES=3 python run_nerf.py --render_only --render_test --config configs/250_blender_chair_8views_ctr.txt &
+CUDA_VISIBLE_DEVICES=4 python run_nerf.py --render_only --render_test --config configs/251_blender_drums_8views_ctr.txt &
+CUDA_VISIBLE_DEVICES=5 python run_nerf.py --render_only --render_test --config configs/252_blender_ficus_8views_ctr.txt &
+wait;
+CUDA_VISIBLE_DEVICES=3 python run_nerf.py --render_only --render_test --config configs/042_blender_paper_lego.txt &
+CUDA_VISIBLE_DEVICES=4 python run_nerf.py --render_only --render_test --config configs/254_blender_mic_8views_ctr.txt &
+CUDA_VISIBLE_DEVICES=5 python run_nerf.py --render_only --render_test --config configs/255_blender_ship_8views_ctr.txt &
+wait;
+CUDA_VISIBLE_DEVICES=3 python run_nerf.py --render_only --render_test --config configs/256_blender_hotdog_8views_ctr.txt &
+CUDA_VISIBLE_DEVICES=4 python run_nerf.py --render_only --render_test --config configs/257_blender_materials_8views_ctr.txt &
+CUDA_VISIBLE_DEVICES=5 python run_nerf.py --render_only --render_test --config configs/258_blender_materials_8views_ctr.txt &
